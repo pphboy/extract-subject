2b2f81f47fc5b2698a498b21629c5d9f7ca5419a
pphboy <pphboy@aliyun.com>
2023-02-12 00:02:11 +0800

    完成答案显示

--- component/paperwidget.cpp
+++ component/paperwidget.cpp
@@ -25,7 +25,6 @@ PaperWidget::PaperWidget(QWidget *parent) : QWidget(parent)
     qsa->setFixedSize(700,760);
     qsa->move(0,40);
 
-
 }
 
 void PaperWidget::closeEvent(QCloseEvent *event)
@@ -36,6 +35,7 @@ void PaperWidget::closeEvent(QCloseEvent *event)
 
 void PaperWidget::refreshPaper(int pid)
 {
+
     // 把题目查出来
     // 这个玩意，每次显示的时候都需要重新绘制，不然就出问题
     paperView = new QWidget; // 题目显示的窗口
@@ -48,6 +48,8 @@ void PaperWidget::refreshPaper(int pid)
     this->setWindowTitle(tr("《%1》卷子详细页").arg(s.getPname()));
     this->paperTitle->setText(tr("《%1》 注意：写完要保存，没写自动保存").arg(s.getPname()));
 
+
+    QTextBrowser *qtshow = new QTextBrowser;
     qDebug() << s.getPname();
     int height;
     for(auto a: s.getCategoryTaskList()){
@@ -56,12 +58,23 @@ void PaperWidget::refreshPaper(int pid)
         // 分类
         paperPaint->addWidget(t);
         for(auto t: a->getTaskList()){
+            paperPaint->addWidget(new QLabel(""));
             height = ceil(t->getT_title().size()/40.00)*26;
             // 题目
+            // 这是一个空格
             QTextBrowser *qtb = new QTextBrowser;
             qtb->setHtml(t->getT_description());
             qtb->setFixedSize(qsa->width()-35,height);
             paperPaint->addWidget(qtb);
+            // 显示答案的按钮
+            QPushButton* qb = new QPushButton("显示答案");
+            connect(qb,&QPushButton::clicked,[=](){
+                qtshow->setHtml(t->getT_content());
+                qtshow->setWindowFlags(Qt::WindowStaysOnTopHint);
+                qtshow->setWindowTitle("答案显示框");
+                qtshow->show();
+            });
+            paperPaint->addWidget(qb);
 
             // 这个应该在查询的时候就初始化好了，因为我需要设置答案
             t->getQte()->setFixedWidth(qsa->width()-35);
